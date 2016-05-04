b5e2321e49434bc47d345065ba6446ce543f3780
Carlos Villa <uo236851@uniovi.es>
2016-05-04 19:41:04 +0200

    Cambio VoteCounting

--- VoteCounting_1a/src/main/java/es/uniovi/asw/Application.java
+++ VoteCounting_1a/src/main/java/es/uniovi/asw/Application.java
@@ -1,17 +1,28 @@
 package es.uniovi.asw;
 
+import java.awt.EventQueue;
+
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import es.uniovi.asw.ui.MainWindow;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Application.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
+	  EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow frame = new MainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
   }
 }
